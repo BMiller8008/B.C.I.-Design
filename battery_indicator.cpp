4ae8aae6103769d2ed74df20c47f1640750ee4b3
Rohit Lala <rohitlala08@gmail.com>
2025-02-18 18:27:23 -0500

    updated battery indicator

--- battery_indicator.cpp
+++ battery_indicator.cpp
@@ -2,44 +2,55 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/adc.h"
+#include "driver/gpio.h"
 
 // ADC channel and width configuration
 #define ADC_CHANNEL ADC1_CHANNEL_0
 #define ADC_WIDTH ADC_WIDTH_BIT_12 
 #define ADC_ATTEN ADC_ATTEN_DB_11  
 
-void adc_task(void *arg) {
-    while (true) {
-        int raw_value = adc1_get_raw(ADC_CHANNEL); 
-        float voltage = (raw_value / 4095.0) * 3.3; // Convert to voltage
-        // printf("Raw ADC Value: %d, Voltage: %.2fV\n", raw_value, voltage);
+class BatteryMonitor {
+public:
+    BatteryMonitor(adc1_channel_t channel) : channel_(channel) {
+        adc1_config_width(ADC_WIDTH);
+        adc1_config_channel_atten(channel_, ADC_ATTEN);
+    }
 
-        //upload to an object so that it can be getted by oled
+    float readVoltage() {
+        int raw_value = adc1_get_raw(channel_);
+        return (raw_value / 4095.0) * 3.3;
+    }
+
+    void printBatteryStatus() {
+        float voltage = readVoltage();
         if (voltage > 2.78) {
             printf("100%%\n");
-        }
-        else if (voltage > 2.26) {
+        } else if (voltage > 2.26) {
             printf("75%%\n");
-        }
-        else if (voltage > 1.73) {
+        } else if (voltage > 1.73) {
             printf("50%%\n");
+        } else if (voltage > 1.21) {
+            printf("Low Battery!\n25%%\n");
+        } else {
+            printf("Shutting Down\n0%%\n");
         }
-        else if (voltage > 1.21) {
-            printf("Low Battery!\n");
-            printf("25%%\n");
-        }
-        else {
-            printf("Shutting Down\n");
-            printf("0%%\n");
-        }
-        
-        vTaskDelay(pdMS_TO_TICKS(500));  // Read every 500ms
+    }
+
+private:
+    adc1_channel_t channel_;
+};
+
+void adc_task(void *arg) {
+    BatteryMonitor *monitor = static_cast<BatteryMonitor*>(arg);
+    
+    while (true) {
+        monitor->printBatteryStatus();
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
 
 extern "C" void app_main() {
-    adc1_config_width(ADC_WIDTH);
-    adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN);
+    static BatteryMonitor monitor(ADC_CHANNEL);
 
-    xTaskCreate(adc_task, "ADC Reader", 4096, NULL, 1, NULL);
+    xTaskCreate(adc_task, "ADC Reader", 4096, &monitor, 1, NULL);
 }
